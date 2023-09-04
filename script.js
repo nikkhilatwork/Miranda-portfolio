d81ebb1923367d9aecc707eb4647c09d33ebfeb8
Nikhil zolekar <rameshmirchandani86@gmail.com>
2023-09-04 23:55:19 +0530

    miranda-portfolio

---
+++ script.js
@@ -0,0 +1,27 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
+
+let tl = gsap.timeline();
+
+
+tl.to("#page1",{
+    y:"100vh",
+    Scale:0.6,
+    duration:0
+})
+tl.to("#page1", {
+    y:"30vh",
+    duration:1,
+    delay:1
+});
+
+tl.to("#page1",{
+    y:"0vh",
+    rotate:360,
+    scale:1, 
+    duration:0.8
+});
